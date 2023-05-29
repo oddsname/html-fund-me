d1a2391d4c652245e086fa09bb56fd8c22dba4a7
oddsname <vadimich2012@mail.ru>
2023-05-29 13:01:42 +0300

    added fund function, added connection to hardhat local blockchain

--- main.js
+++ main.js
@@ -1,24 +1,73 @@
 import './style.css'
+import {ethers} from "ethers";
+import {abi, contractAddress} from "./consts.js";
+
+const connectBtn = document.querySelector('#connect');
+const fundBtn = document.querySelector('#fund');
+
+if (typeof window.ethereum === "undefined") {
+    console.error('Please install Metamask')
+    document.querySelector('h1').innerHTML = 'Please install Metamask';
+    throw new Error('Metamask not found')
+}
 
 const connectMetamask = async () => {
     //only for metamask
-    if(typeof window.ethereum !== "undefined") {
-        return await window.ethereum.request({
-                method: "eth_requestAccounts"
-        });
-    }
-
-    return false;
+    return await window.ethereum.request({
+        method: "eth_requestAccounts"
+    });
 }
 
-document.querySelector('button')
-    .addEventListener('click', async function (e) {
-        e.preventDefault();
+const fund = async (ethAmount = '0.1') => {
+    // to call fund function we need these thing
+    // provider / connection to the blockchain
+    // signer / wallet / someone with the gas
+    // contract that we are interaction with
 
-        if(await connectMetamask()) {
-            e.target.innerHTML = "Connected";
-        } else {
-            e.target.innerHTML = "Please install metamask";
-        }
+    //here we installed ethers 5.7.2 in future releases I didn't find Web3Provider anymore
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    const ethValue = ethers.utils.parseEther(ethAmount);
+
+    const txResponse = await contract.fund({ value: ethValue});
+
+    await listForTxMine(txResponse, provider);
+    console.log('Done');
+}
+
+const listForTxMine = async (txResponse, provider) => {
+    console.log(`Mining ${txResponse.hash}`);
+
+    return new Promise((resolve) => {
+        //event listener that triggers only once
+        provider.once(txResponse.hash, (txReceipt) => {
+            console.log(`completed with ${txReceipt.confirmations}`);
+            resolve(txReceipt);
+        });
     })
+}
+
+connectBtn.addEventListener('click', async function (e) {
+    e.preventDefault();
+
+    try {
+        if (await connectMetamask()) {
+            connectBtn.innerHTML = "Connected";
+        }
+    } catch (e) {
+        console.error(e);
+    }
+})
+
+fundBtn.addEventListener('click', async function (e) {
+    e.preventDefault();
+
+    try {
+        await fund();
+    } catch (e) {
+        console.error(e);
+    }
+})
 
